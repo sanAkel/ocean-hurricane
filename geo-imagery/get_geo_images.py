b02d3b1abf2072c8298ae4b7a5fb640c2472fbc2
emon-c <135040774+emon-c@users.noreply.github.com>
2024-06-20 16:07:55 -0400

    Update get_geo_images.py
    Modified structure of code to make it slightly more efficient in downloading

--- geo-imagery/get_geo_images.py
+++ geo-imagery/get_geo_images.py
@@ -7,61 +7,50 @@ import shutil
 import pandas as pd
 
 def download_file(url, output_fName):
-  # https://stackoverflow.com/questions/17285464/whats-the-best-way-to-download-file-using-urllib3
-  http = urllib3.PoolManager()
-  with open(output_fName, 'wb') as out:
-    r = http.request('GET', url, preload_content=False)
-    shutil.copyfileobj(r, out)
+    http = urllib3.PoolManager()
+    with http.request('GET', url, preload_content=False) as response, open(output_fName, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+    
+    print(f"\n\nDownloaded: {output_fName}")
+    
+    if not os.path.isfile(output_fName):
+        print(f"\n\nError in downloading file: {output_fName}")
+        print(f"Check URL:\n{url}\n\n")
 
-  print("\n\nDownloaded: {}".format(output_fName))
+def construct_url(date, hour, key, sat, size, lat_lon, cov, band):
+    base_url = "https://mcfetch.ssec.wisc.edu/cgi-bin/mcfetch?dkey="
+    url = (f"{base_url}{key}&satellite={sat}&output=PNG&size={size}&lat={lat_lon}"
+           f"&MAP=YES&GEORES=YES&gray=YES&date={date}&time={hour}:01:17&coverage={cov}&band={band}")
+    return url
 
-  if not os.path.isfile(output_fName):
-    print("\n\nError in downloading file:{}".format(output_fName))
-    print("Check URL:\n{}\n\n".format(url))
-# --
+def main():
+    parser = argparse.ArgumentParser(
+        prog='get_geo_images.py',
+        description='Download Geostationary satellite imagery between start - end dates.',
+        usage='%(prog)s [options]'
+    )
+    
+    parser.add_argument('--start_date', type=str, metavar='starting date', default="20210827")
+    parser.add_argument('--end_date', type=str, metavar='ending date', default="20210828")
+    args = parser.parse_args()
+    
+    key = "o569q7mv-oftx-sdoz-kbat-9ll6heyu8r8j"
+    lat_lon = "25.0+75.2"
+    size = "216+512"
+    sat = "GOES16"
+    cov = "FD"
+    band = "14"
+    
+    start_date, end_date = args.start_date, args.end_date
+    output_dir = "/Users/emonc/Documents/internship2024/figs_ida/"
+    
+    for date in pd.date_range(start_date, end_date):
+        date_str = date.strftime('%Y%m%d')
+        for hour in range(24):
+            utc_hour = str(hour).zfill(2)
+            url = construct_url(date_str, utc_hour, key, sat, size, lat_lon, cov, band)
+            output_fName = f"{sat}_b_{band}_{date_str}_{utc_hour}z.png"
+            download_file(url, os.path.join(output_dir, output_fName))
 
-# fixed parameters
-myKEY = "yug2253g-jeua-rbjy-l0x1-ntrk21rqve56"
-LAT_LON = "25.0+75.2"
-SIZE = "216+512"
-SAT = "GOES16"
-COV = "FD" #"CONUS"
-BAND = str(14)
-base_url = "https://mcfetch.ssec.wisc.edu/cgi-bin/mcfetch?dkey="
-# --
-
-# user inputs
-get_inputs = argparse.ArgumentParser(prog='\nget_geo_images.py',\
-description='Download Geostationary satellite imagery between start - end dates.', usage='%(prog)s [options]')
-
-get_inputs.add_argument('--start_date', type=str,\
-                        metavar='starting date', default="20230825")
-
-get_inputs.add_argument('--end_date', type=str,\
-                        metavar='ending date', default="20230901")
-args = get_inputs.parse_args()
-# --
-
-date_s, date_e = [args.start_date, args.end_date]
-for dd in pd.date_range(date_s, date_e):
-  #print( dd.strftime('%Y%m%d'))
-
-  for hr in range(0, 24):
-    UTC_hr = str(hr).zfill(2)
-
-    url = base_url+myKEY+\
-          "&satellite=" + SAT+\
-          "&output=PNG"+\
-          "&size=" + SIZE+\
-          "&lat=" + LAT_LON+\
-          "&MAP=YES&GEORES=YES&gray=YES" +\
-          "&date=" + dd.strftime('%Y%m%d')+\
-          "&time=" + UTC_hr + ':01:17'\
-          "&coverage=" + COV+\
-          "&band=" + BAND
-
-    #print(url)
-    output_fName = SAT + "_b_"+BAND + "_" + dd.strftime('%Y%m%d') + "_" + UTC_hr + "z.png"
-    output_path = "/Users/sakella/Desktop/mm/pp/qq/figs/"
-    #print(output_path+output_fName)
-    download_file(url, output_path+output_fName)
+if __name__ == "__main__":
+    main()
